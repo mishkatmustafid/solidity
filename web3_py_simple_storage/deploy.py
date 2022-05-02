2f23411ee117f7da7bbdb7d0234622a57232e35a
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-05-02 19:16:48 +0600

    web3 added

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,5 +1,7 @@
 import json
+import os
 from solcx import compile_standard, install_solc
+from web3 import Web3
 
 with open('./SimpleStorage.sol','r') as file:
   simple_storage_file = file.read()
@@ -28,3 +30,19 @@ compiled_sol = compile_standard(
 
 with open("compiled_code.json","w") as file:
   json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol['contracts']['SimpleStorage.sol']['SimpleStorage']['evm']['bytecode']['object']
+
+# get abi
+abi = compiled_sol['contracts']['SimpleStorage.sol']['SimpleStorage']['abi']
+
+# for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0xA26Fd43C4b79f85BD928148172BA1AA4A6c2088D"
+private_key = os.getenv("private_key")
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print(SimpleStorage)
