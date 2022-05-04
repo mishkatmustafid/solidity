7d826b0003913e192824123ffa6239fb9c096891
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-05-04 06:12:18 +0600

    Lesson 4 - complete

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -2,6 +2,9 @@ import json
 import os
 from solcx import compile_standard, install_solc
 from web3 import Web3
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with open('./SimpleStorage.sol','r') as file:
   simple_storage_file = file.read()
@@ -37,12 +40,49 @@ bytecode = compiled_sol['contracts']['SimpleStorage.sol']['SimpleStorage']['evm'
 # get abi
 abi = compiled_sol['contracts']['SimpleStorage.sol']['SimpleStorage']['abi']
 
-# for connecting to ganache
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id = 1337
-my_address = "0xA26Fd43C4b79f85BD928148172BA1AA4A6c2088D"
-private_key = os.getenv("private_key")
+# for connecting to rinkeby
+w3 = Web3(Web3.HTTPProvider("https://rinkeby.infura.io/v3/710740cf168546b5aab1c83a18dcea50"))
+chain_id = 4
+my_address = "0x3afDC40124BC063246517830eddF55016A778877"
+private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
-print(SimpleStorage)
+# Get the lastest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+# 1. Build a transaction
+# 2. Sign the transaction
+# 3. Send the transaction
+transaction = SimpleStorage.constructor().buildTransaction({
+  "chainId": chain_id,
+  "from": my_address,
+  "nonce": nonce,
+  "gasPrice": w3.eth.gas_price,
+})
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+# Send the signed transaction
+print("Deploying contract...")
+tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+print("Deployed!")
+# Working with the contract, you always need
+# Contract address
+# Contract ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# Call -> Simulate making the call and getting a return value
+# Transact -> Actually make a state change
+
+# Initial value of favorite number
+print(simple_storage.functions.retrieve().call())
+print("Updating Contract...")
+store_transaction = simple_storage.functions.store(42).buildTransaction({
+  "chainId": chain_id,
+  "from": my_address,
+  "nonce": nonce + 1,
+  "gasPrice": w3.eth.gas_price,
+})
+signed_txn = w3.eth.account.sign_transaction(store_transaction, private_key=private_key)
+send_store_tx = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.waitForTransactionReceipt(send_store_tx)
+print("Updated!")
+print(simple_storage.functions.retrieve().call())
