6d7dd4adf9eb2ec1afb14bc4b284a095fa487f6e
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-05-04 14:58:21 +0600

    Lesson 5 - complete

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 def deploy_simple_storage():
   # print(account)
@@ -7,7 +7,7 @@ def deploy_simple_storage():
   # account = accounts.add(config['wallets']['from_key'])
   # print(account)
 
-  account = accounts[0]
+  account = get_account()
   simple_storage = SimpleStorage.deploy({'from': account})
   stored_value = simple_storage.retrieve()
   print(stored_value)
@@ -15,6 +15,12 @@ def deploy_simple_storage():
   transaction.wait(1)
   updated_stored_value = simple_storage.retrieve()
   print(updated_stored_value)
+
+def get_account():
+  if(network.show_active() == 'development'):
+    return accounts[0]
+  else:
+    return accounts.add(config['wallets']['from_key'])
 
 def main():
   deploy_simple_storage()
---
+++ brownie_simple_storage/scripts/read_value.py
@@ -0,0 +1,9 @@
+from brownie import SimpleStorage, accounts, config
+
+def read_contract():
+  simple_storage = SimpleStorage[0]
+  # go take the index thats one less than the length
+  print(simple_storage.retrieve())
+
+def main():
+  read_contract()
