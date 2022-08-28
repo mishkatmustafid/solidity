ae3325df44f7178ded1df7108886a31f16678270
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-08-29 01:36:43 +0600

    web3_py_simple_storage script updated

--- 1_web3_py_simple_storage/deploy.py
+++ 1_web3_py_simple_storage/deploy.py
@@ -6,65 +6,69 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-with open('./SimpleStorage.sol','r') as file:
-  simple_storage_file = file.read()
-  
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
 # Compile Our Solidity
 
-install_solc('0.6.0')
+install_solc("0.6.0")
 compiled_sol = compile_standard(
-  {
-    "language": "Solidity",
-    "sources": {
-      "SimpleStorage.sol": {
-        "content": simple_storage_file
-      }
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
     },
-    "settings": {
-      "outputSelection": {
-        "*": {
-          "*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]
-        }
-      }
-    }
-  },
-  solc_version = '0.6.0'
+    solc_version="0.6.0",
 )
 
-with open("compiled_code.json","w") as file:
-  json.dump(compiled_sol, file)
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
 
 # get bytecode
-bytecode = compiled_sol['contracts']['SimpleStorage.sol']['SimpleStorage']['evm']['bytecode']['object']
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
 
 # get abi
-abi = compiled_sol['contracts']['SimpleStorage.sol']['SimpleStorage']['abi']
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # for connecting to rinkeby
-w3 = Web3(Web3.HTTPProvider("https://rinkeby.infura.io/v3/710740cf168546b5aab1c83a18dcea50"))
+w3 = Web3(
+    Web3.HTTPProvider("https://rinkeby.infura.io/v3/710740cf168546b5aab1c83a18dcea50")
+)
 chain_id = 4
 my_address = "0x3afDC40124BC063246517830eddF55016A778877"
 private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
-# Get the lastest transaction
+
+# Get the latest transaction
 nonce = w3.eth.getTransactionCount(my_address)
+
 # 1. Build a transaction
 # 2. Sign the transaction
 # 3. Send the transaction
-transaction = SimpleStorage.constructor().buildTransaction({
-  "chainId": chain_id,
-  "from": my_address,
-  "nonce": nonce,
-  "gasPrice": w3.eth.gas_price,
-})
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+        "gasPrice": w3.eth.gas_price,
+    }
+)
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
 # Send the signed transaction
 print("Deploying contract...")
 tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
 print("Deployed!")
+
 # Working with the contract, you always need
 # Contract address
 # Contract ABI
@@ -75,12 +79,15 @@ simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
 # Initial value of favorite number
 print(simple_storage.functions.retrieve().call())
 print("Updating Contract...")
-store_transaction = simple_storage.functions.store(42).buildTransaction({
-  "chainId": chain_id,
-  "from": my_address,
-  "nonce": nonce + 1,
-  "gasPrice": w3.eth.gas_price,
-})
+# Create transaction -> Sign transaction -> Send transaction -> Get receipt
+store_transaction = simple_storage.functions.store(42).buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce + 1,
+        "gasPrice": w3.eth.gas_price,
+    }
+)
 signed_txn = w3.eth.account.sign_transaction(store_transaction, private_key=private_key)
 send_store_tx = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 tx_receipt = w3.eth.waitForTransactionReceipt(send_store_tx)
