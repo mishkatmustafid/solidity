da45ffa0ec1591e758cc212986734ad8d2f6e708
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-11-10 16:29:57 +0600

    DappToken contract - updated

--- defi_stake_yield_brownie/contracts/DappToken.sol
+++ defi_stake_yield_brownie/contracts/DappToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DappToken is ERC20 {
-    constructor() public ERC20("Dapp Token", "DAPP") {
-        _mint(msg.sender, 1000000000000000000000000);
+    constructor() ERC20("Dapp Token", "DAPP") {
+        _mint(msg.sender, 1000000000000000000);
     }
 }
